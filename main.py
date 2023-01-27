14e2df20c006d97c5ade82f49305951efc1b9afb
Eddie Zhuang <53500062+EdZ543@users.noreply.github.com>
2023-01-27 23:44:30 +0000

    Fix verification code

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ VERIFIED_ROLE_NAME = "verified"
 
 # Temporary array of allowed users. Should get these from the google sheet in the future.
 temp_allowed_users = [
-	"EdZ123#8965"
+	"EdZ123#8965",
+	"RZ Music#5601"
 ]
 
 intents = discord.Intents.default()
@@ -30,16 +31,22 @@ async def on_message(message):
 
 # Verify new members
 @client.event
-async def on_member_join(self, member):
+async def on_member_join(member):
 	guild = member.guild
 	verified_role = discord.utils.get(guild.roles, name=VERIFIED_ROLE_NAME)
 	
 	if guild.system_channel is not None:
-		if f'{member.username}#{member.id}' in temp_allowed_users:
-			await member.add_roles(verified_role, reason="Member is verified")
-			to_send = f'Welcome {member.mention} to {guild.name}!'
+		if f'{member.name}#{member.discriminator}' in temp_allowed_users:
+			try:
+				await member.add_roles(verified_role)
+			except Exception as e:
+				print(e)
+				to_send = "Error assigning roles."
+			else:
+				to_send = f'Welcome {member.mention} to {guild.name}, you have been verified!'
 		else:
-			to_send = f'Hello {member.mention}, your username and ID are not in our allowed list. If you changed your username or ID after registering, please email us.'
+			to_send = f'Hello {member.mention}, your username and tag are not in our allowed list. If you changed your username or tag after registering, please email us.'
+			
 	await guild.system_channel.send(to_send)
 
 client.run(os.getenv('TOKEN'))
