c8adc87aa1abb91798ae729225595c45ef229054
Eddie Zhuang <53500062+EdZ543@users.noreply.github.com>
2023-01-27 20:28:39 +0000

    Add gateway intent

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# This code is based on the following example:
-# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
-
-import discord
 import os
+import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
@@ -18,11 +18,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-try:
-    client.run(os.getenv("TOKEN"))
-except discord.HTTPException as e:
-    if e.status == 429:
-        print("The Discord servers denied the connection for making too many requests")
-        print("Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests")
-    else:
-        raise e
+client.run(os.getenv('TOKEN'))
