46e1bfb0524d1ef54188bb353a7e9d1e823218b0
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-02-03 09:24:58 -0500

    Move to the YRHacks account project

--- extensions/invitation.py
+++ extensions/invitation.py
@@ -1,8 +1,16 @@
+import os
 from dotenv import load_dotenv
-import discord
 from discord.ext import commands
 
 load_dotenv()
+
+# Constants
+MESSAGE = """
+Hi! Please click the link below to join the YRHacks Discord server.
+
+{link}
+"""
+SUBJECT = "YRHacks Discord Server Invitation"
 
 
 class Invitation(commands.Cog):
@@ -10,6 +18,7 @@ class Invitation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.drafts = []
 
     async def generate_invite_link(self, ctx):
         """Generate a one-time invite link"""
@@ -17,14 +26,16 @@ class Invitation(commands.Cog):
         invite = await channel.create_invite(max_uses=1)
         return invite.url
 
-    def create_draft(self, ctx, email_address):
+    async def create_draft(self, ctx, email_address):
         """Create a draft invitation link for a member"""
         pass
 
     @commands.command()
     async def create_drafts(self, ctx):
         """Create drafts for all members"""
-        pass
+
+        if self.service == None:
+            self.auth_google_api()
 
     @commands.command()
     async def send_drafts(self, ctx):
@@ -33,8 +44,7 @@ class Invitation(commands.Cog):
 
     @commands.command()
     async def test(self, ctx):
-        link = await self.generate_invite_link(ctx)
-        print(link)
+        self.create_drafts(ctx)
 
 
 async def setup(bot):
--- extensions/verification.py
+++ extensions/verification.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import tasks, commands
-
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -53,8 +55,24 @@ class Verification(commands.Cog):
     def auth_google_api(self):
         """Authenticate Google Sheets API"""
 
-        service = build('sheets', 'v4')
-        self.sheet = service.spreadsheets()
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        self.service = build('sheets', 'v4', credentials=creds)
 
     @tasks.loop(seconds=UPDATE_INTERVAL)
     async def update_users(self):
@@ -62,8 +80,8 @@ class Verification(commands.Cog):
 
         try:
             # Call the Sheets API
-            result = self.sheet.values().get(spreadsheetId=os.getenv('SPREADSHEET_ID'),
-                                             range=os.getenv('SPREADSHEET_RANGE')).execute()
+            result = self.service.spreadsheets().values().get(spreadsheetId=os.getenv('SPREADSHEET_ID'),
+                                                              range=os.getenv('SPREADSHEET_DISCORD_RANGE')).execute()
             values = result.get('values', [])
             self.approved_users = [value[0] for value in values]
         except HttpError as err:
