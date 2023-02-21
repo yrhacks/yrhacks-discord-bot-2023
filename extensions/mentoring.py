4fa7fff6bf521cb028b610833ca596633660a4e8
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-02-21 18:39:28 -0500

    Visually separate requests

--- extensions/mentoring.py
+++ extensions/mentoring.py
@@ -4,16 +4,30 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from discord import app_commands
 import traceback
+import textwrap
 
 load_dotenv()
 
 
 class RequestView(discord.ui.View):
-    def __init__(self):
+    def __init__(self, requester: discord.Interaction.user, request_subject):
         super().__init__(timeout=None)
+        self.requester = requester
+        self.request_subject = request_subject
 
     @discord.ui.button(label="Accept", style=discord.ButtonStyle.green, custom_id='accept')
     async def accept(self, interaction: discord.Interaction, button: discord.ui.Button):
+        channel_name = f"{interaction.user.name} - {self.requester.name} - {self.request_subject}"
+        mentoring_category = interaction.guild.get_channel(
+            int(os.getenv('MENTORING_CATEGORY_ID')))
+        channel = await interaction.guild.create_text_channel(
+            name=channel_name, category=mentoring_category)
+
+        await channel.send(textwrap.dedent(f"""
+        Hello, {self.requester.mention}! {interaction.user.mention} has accepted your request for help concerning {self.request_subject}!
+        Please use this channel to discuss your questions and concerns.
+        """))
+
         button.disabled = True
 
 
@@ -29,10 +43,11 @@ class RequestModal(discord.ui.Modal, title="Mentor Request"):
         requests_channel = interaction.guild.get_channel(
             int(os.getenv('REQUESTS_CHANNEL_ID')))
         request_string = f"""
-    Request from {interaction.user.mention}
-    Subject: {self.subject_input.value}
+        Request from {interaction.user.mention}
+        Subject: {self.subject_input.value}
         """
-        await requests_channel.send(request_string, view=RequestView())
+        await requests_channel.send(textwrap.dedent(request_string), view=RequestView(interaction.user, self.subject_input.value))
+        await requests_channel.send("--------------------")
 
         await interaction.response.send_message('Your request has been submitted! You will be pinged when a mentor accepts it.', ephemeral=True)
 
